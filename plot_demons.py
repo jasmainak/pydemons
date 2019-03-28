22817aae544748725263170df0552c7fd034d8c2
Mainak Jas <mainakjas@gmail.com>
2019-03-27 22:42:06 -0400

    Try for our images

--- plot_demons.py
+++ plot_demons.py
@@ -1,50 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from pydemons import demons
+from pydemons import demons, iminterpolate
+from PIL import Image
+
+moving = np.array(Image.open("mdb001.pgm"), dtype=np.float)
+fixed = np.array(Image.open("mdb003.pgm"), dtype=np.float)
 
 
-def run_demons(moving, fixed, **kwargs):
-    # plot input images
-    plt.ion()
-    plt.figure(figsize=(13.5, 7))
-    plt.gray()
-    ax = plt.subplot(141)
-    ax.set_title("fixed")
-    plt.axis("off")
-    ax.imshow(fixed)
-    ax = plt.subplot(142)
-    ax.set_title("moving")
-    plt.axis("off")
-    ax.imshow(moving)
+def remove_padding(im):
+    th = 5000
+    diff_pix = np.diff(np.sum(im, axis=0)[::-1])
+    idx = np.where(diff_pix > th)[0][0]
+    im = np.fliplr(im)
+    im[:, 1:-idx + 1] = im[:, idx:]
+    return im
 
-    # run demons
-    warped = moving
-    diff = warped - fixed
-    ax = plt.subplot(143)
-    ax.set_title("warped")
-    ax.axis("off")
-    warped_thumb = ax.imshow(warped)
-    ax = plt.subplot(144)
-    ax.set_title("diff")
-    ax.axis("off")
-    diff_thumb = ax.imshow(diff)
-    plt.show()
 
-    def _callback(variables):
-        warped = variables["warped"]
-        fixed = variables['fixed']
-        diff = warped - fixed
-        warped_thumb.set_data(warped)
-        plt.draw()
-        diff_thumb.set_data(diff)
-        plt.draw()
+fixed = remove_padding(fixed)
+moving = remove_padding(moving)
 
-    return demons(fixed, moving, callback=_callback, **kwargs)
+sx, sy, vx, vy = demons(fixed, moving, stop_criterion=1e-3)
+warped = iminterpolate(moving, sx=sx, sy=sy)
 
-if __name__ == "__main__":
-    # load data
-    from PIL import Image
-    fixed = np.array(Image.open("data/lenag2.png"), dtype=np.float)
-    moving = np.array(Image.open("data/lenag1.png"),
-                      dtype=np.float)
-    run_demons(moving, fixed)
+plt.figure()
+plt.title('Fixed')
+plt.imshow(fixed)
+plt.show()
+
+plt.figure()
+plt.title('Moving')
+plt.imshow(moving)
+plt.show()
+
+plt.figure()
+plt.title('Warped')
+plt.imshow(warped)
+plt.show()
